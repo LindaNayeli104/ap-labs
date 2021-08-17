473daab5ca20866488c0b3990826e21e3440e52c
Linda Abundis <linda_nayeli104@hotmail.com>
2021-08-17 23:38:53 +0000

    Solve test-lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -6,6 +6,17 @@ import (
 )
 
 func main() {
-	name := os.Args[1]
-	fmt.Printf("Welcome %v to the jungle \n", name)
+	name := os.Args[1:]
+	
+	if(len(name) == 0){
+		fmt.Printf("Please enter the required parameter \n")
+	}else{
+		fmt.Printf("Welcome ")
+		for i := 0; i < len(name); i++{
+			fmt.Printf(name[i] + " ")
+		}	
+		fmt.Printf("to the jungle \n")
+	}
 }
+
+
