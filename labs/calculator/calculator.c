1839b768843a74ee4d5c3eeab9e8654e20342fc6
Linda Abundis <linda_nayeli104@hotmail.com>
2021-09-01 04:30:30 +0000

    advances of the lab

--- labs/calculator/calculator.c
+++ labs/calculator/calculator.c
@@ -1,47 +1,64 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 // adds/subtracts/multiplies all values that are in the *values array.
 // nValues is the number of values you're reading from the array
 // operator will indicate if it's an addition (1), subtraction (2) or
 // multiplication (3)
 long calc(int operator, int nValues, int *values) {
-    int res;
-    for(int i=0; i<nValues; i++){
+    int res = res = values[0];
+
+    for(int i=1; i<nValues; i++){
         if(operator == 1){
-            res -= values[i];
-        }else if(operator == 2){
             res += values[i];
+        }else if(operator == 2){
+            res -= values[i];
         }else if(operator == 3){
             res *= values[i];
         }
     }
-    return 0;
+    return res;
 }
 
-int main() {
-    char operacion;
-    int nums[5]; // cambiar esto
-    int operacionNum;
-    int nValues;
-    scanf("%s", &operacion);
-    for(int i=0; i<sizeof nums; i++){
-        //Agregar validacion de numeros
-        scanf("%d",&nums[i]);
-    }
 
-    if(strcmp(&operacion, "add") == 0){
+int main(int argc, char **argv){
+    char *operacion = argv[1];
+    int operacionNum;
+    int nValues = argc-2;
+    int nums[nValues]; // cambiar esto
+    
+    for(int i=0; i<nValues; i++){
+        //Agregar validacion de numeros
+        if(strcmp(argv[i+2], "0") && ){
+
+        }
+        nums[i] =  atoi(argv[i+2]);
+    }
+  
+    if(strcmp(operacion, "add") == 0){
         operacionNum = 1;
-    }else if(strcmp(&operacion, "sub") == 0){
+    }else if(strcmp(operacion, "sub") == 0){
         operacionNum = 2;
-    }else if(strcmp(&operacion, "mult") == 0){
+    }else if(strcmp(operacion, "mult") == 0){
         operacionNum = 3;
     }else{
-        //Mandar error
-        printf("Operador no valido");
-        return 0;  //Checar
+        printf("Operador no valido\n");
+        return 0;
     }
-    nValues = sizeof nums;
+
     long res = calc(operacionNum, nValues, nums);   // se pasa como nums o *nums
-    printf("%ld", res);
+    for(int i=0; i<nValues-1; i++){
+        //Agregar validacion de numeros
+        if(operacionNum == 1){
+            printf("%d + ", nums[i]);
+        }else if(operacionNum == 2){
+            printf("%d - ", nums[i]);
+        }else if(operacionNum == 3){
+            printf("%d * ", nums[i]);
+        }
+    }
+    printf("%d = %ld\n", nums[nValues-1], res);
+    //printf("La respuesta es: %ld\n", res);
+    return 0;
 }
---
+++ labs/calculator/pruebas.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+// adds/subtracts/multiplies all values that are in the *values array.
+// nValues is the number of values you're reading from the array
+// operator will indicate if it's an addition (1), subtraction (2) or
+// multiplication (3)
+
+
+
+int main(int argc, char **argv){
+    printf("%s", argv[2]);
+    //printf("La respuesta es: %ld", res);
+    return 0;
+}
